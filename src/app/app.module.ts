50cc90dbf3234d89f1f4cd7abaf43f239f4f1abe
Dean Kago <martokhago@gmail.com>
2022-04-17 19:25:09 +0300

    added main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CustomDirectiveDirective } from './custom-directive.directive';
+import { CustomDirectiveDirective } from './Directives/custom-directive.directive';
 import { MainComponent } from './components/main/main.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProfileComponent } from './components/profile/profile.component';
