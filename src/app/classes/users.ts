7bcc5cbeae65c544afdddae5456c50f8222e2ea1
Dean Kago <martokhago@gmail.com>
2022-04-18 15:51:01 +0300

    added users and repos class

---
+++ src/app/classes/users.ts
@@ -0,0 +1,5 @@
+export class Users {
+  constructor(public avator: string, public userName: string, public bio: string, public followers: number, public following: number, public home: string ){
+    
+  }
+}
